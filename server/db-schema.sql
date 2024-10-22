a35b115a93ec5e9982db60bf55c74775566a03e0
Zach Maddox <zmaddox@jbssolutions.com>
2024-10-22 18:45:44 -0400

    schema fix

--- server/db-schema.sql
+++ server/db-schema.sql
@@ -1,8 +1,8 @@
 create table readings (
     sensor_id bigint not null,
     ts timestamp with time zone not null,
-    temperature decimal(4,2),
-    humidity decimal(4,2)
+    temperature decimal(5,2),
+    humidity decimal(5,2)
 );
 
 create unique index on readings (sensor_id, ts);
